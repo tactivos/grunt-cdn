13bdba2e61a2b2f5d37156752294bef9f8d299ee
Juan Pablo Garcia <juanpablogarcia@gmail.com>
2012-12-25 22:44:46 -0300

    Improved html regex

--- tasks/grunt-cdn.js
+++ tasks/grunt-cdn.js
@@ -18,7 +18,7 @@ module.exports = function (grunt) {
 		ejs: 'html'
 	};
 
-	var reghtml = new RegExp(/<[img|link|source].*[src|href]=['"]([\/][^'"]+)['"].*>/ig);
+	var reghtml = new RegExp(/<[^a].*[src|href]=['"]([\/][^'"]+)['"].*>/ig);
 
 	var regcss = new RegExp(/url\(([^)]+)\)/ig);
 
