67074ba2d4af874a52fcfcbb203ed0ace35d46ec
RobinQu <robinqu@gmail.com>
2013-12-16 10:58:07 +0800

    lint free

--- tasks/grunt-cdn.js
+++ tasks/grunt-cdn.js
@@ -6,18 +6,10 @@
  */
 module.exports = function(grunt) {
 
-  var fs = require('fs');
+  // var fs = require('fs');
   var url = require('url');
   var path = require('path');
-  var crypto = require('crypto');
-
-  var supportedTypes = {
-    html: 'html',
-    css: 'css',
-    soy: 'html',
-    ejs: 'html',
-    hbs: 'html'
-  };
+  // var crypto = require('crypto');
 
   var htmlsplitters = [
     {
@@ -37,93 +29,8 @@ module.exports = function(grunt) {
   var regcss = new RegExp(/url\(([^)]+)\)/ig);
   var regcssfilter = new RegExp(/filter[\w\.\:]+\(src=['"]([^'"]+)['"]/ig);
   var ignorePath = null;
-
-  grunt.registerMultiTask('cdn', "Properly prepends a CDN url to those assets referenced with absolute paths (but not URLs)", function() {
-    var self = this;
-    var files = this.files;
-    var options = this.options();
-    var relativeTo = this.options().cdn;
-    ignorePath = this.options().ignorePath;
-
-    var supportedTypes = {
-      html: 'html',
-      css: 'css',
-      soy: 'html',
-      ejs: 'html',
-      hbs: 'html'
-    };
-
-    for(var key in options.supportedTypes){
-      supportedTypes[key] = options.supportedTypes[key];
-    }
-
-    files.forEach(function(file) {
-      file.src.forEach(function (filepath) {
-	var type = path.extname(filepath).replace(/^\./, ''),
-	filename = path.basename(filepath),
-	destfile = (file.dest && file.dest !== filepath) ? path.join(file.dest, filename) : filepath,
-	content = grunt.file.read(filepath);
-	content = content.toString(); // sometimes css is interpreted as object
-
-	if (!supportedTypes[type]) { //next
-	  console.warn("unrecognized extension:" + type + " - " + filepath);
-	  return;
-	}
-
-	grunt.log.subhead('cdn:' + type + ' - ' + filepath);
-
-	if (supportedTypes[type] == "html") {
-	  content = html.call(self, content, filepath, relativeTo);
-	} else if (supportedTypes[type] === "css") {
-	  content = css.call(self, content, filepath, relativeTo);
-	}
-
-	// write the contents to destination
-	grunt.file.write(destfile, content);
-      });
-    });
-  });
-
-  function html(content, filename, relativeTo) {
-    var self = this;
-    return htmlsplitters.reduce(function (value, htmlsplitter) {
-      for (i=0; i<htmlsplitter.splitters.length; i++) {
-        value = value.split(htmlsplitter.splitters[i]);
-        for (j=1; j<value.length; j++) {
-          value[j] = value[j].replace(htmlsplitter.rgx, function(match, resource) {
-	    return match.replace(resource, cdnUrl.call(self, resource, filename, relativeTo));
-          });
-        }
-        value = value.join(htmlsplitter.splitters[i]);
-      }
-      return value;
-    }, content);
-  }
-
-  function css(content, filename, relativeTo) {
-    var self = this,
-    getUrl = function (resource) {
-      resource = resource.replace(/^['"]/, '').replace(/['"]$/, '');
-      var url = cdnUrl.call(self, resource, filename, relativeTo);
-      return url;
-    };
-    return content.replace(regcss, function(attr, resource) {
-      var url = getUrl(resource);
-      if (!url) return attr;
-
-      return grunt.template.process("url('<%= url %>')", {
-	data: {
-	  url: url
-	}
-      });
-    }).replace(regcssfilter, function (rule, resource) {
-      var url = getUrl(resource);
-      if (!url) return rule;
-      
-      return rule.replace(resource, url);
-    });
-  }
-
+  
+  
   function cdnUrl(resource, filename, relativeTo) {
     var options = this.options();
     // skip those absolute urls
@@ -188,10 +95,109 @@ module.exports = function(grunt) {
     grunt.log.writeln('Changing ' + resourceUrl.pathname.cyan + ' -> ' + src.cyan);
     return grunt.template.process("<%= url %><%= search %><%= hash %>", {
       data: {
-	url: src,
-	hash: (resourceUrl.hash || ''), // keep the original hash too
-	search: (resourceUrl.search || '') // keep the original querystring
+      url: src,
+      hash: (resourceUrl.hash || ''), // keep the original hash too
+      search: (resourceUrl.search || '') // keep the original querystring
       }
     });
   }
+
+  function html(content, filename, relativeTo) {
+    var self = this, i, j, replacer;
+    
+    replacer = function(match, resource) {
+      return match.replace(resource, cdnUrl.call(self, resource, filename, relativeTo));
+    };
+    return htmlsplitters.reduce(function (value, htmlsplitter) {
+      for (i=0; i<htmlsplitter.splitters.length; i++) {
+        value = value.split(htmlsplitter.splitters[i]);
+        for (j=1; j<value.length; j++) {
+          value[j] = value[j].replace(htmlsplitter.rgx, replacer);
+        }
+        value = value.join(htmlsplitter.splitters[i]);
+      }
+      return value;
+    }, content);
+  }
+
+  function css(content, filename, relativeTo) {
+    var self = this,
+    getUrl = function (resource) {
+      resource = resource.replace(/^['"]/, '').replace(/['"]$/, '');
+      var url = cdnUrl.call(self, resource, filename, relativeTo);
+      return url;
+    };
+    return content.replace(regcss, function(attr, resource) {
+      var url = getUrl(resource);
+      if (!url) {
+        return attr;
+      }
+
+      return grunt.template.process("url('<%= url %>')", {
+  data: {
+    url: url
+  }
+      });
+    }).replace(regcssfilter, function (rule, resource) {
+      var url = getUrl(resource);
+      if (!url) {
+        return rule;
+      }
+      
+      return rule.replace(resource, url);
+    });
+  }
+  
+
+  grunt.registerMultiTask('cdn', "Properly prepends a CDN url to those assets referenced with absolute paths (but not URLs)", function() {
+    var self = this;
+    var files = this.files;
+    var options = this.options();
+    var relativeTo = this.options().cdn;
+    var key;
+    ignorePath = this.options().ignorePath;
+
+    var supportedTypes = {
+      html: 'html',
+      css: 'css',
+      soy: 'html',
+      ejs: 'html',
+      hbs: 'html'
+    };
+
+    for(key in options.supportedTypes){
+      if(options.supportedTypes.hasOwnProperty(key)) {
+        supportedTypes[key] = options.supportedTypes[key];
+      }
+    }
+
+    files.forEach(function(file) {
+      file.src.forEach(function (filepath) {
+        var type = path.extname(filepath).replace(/^\./, ''),
+        filename = path.basename(filepath),
+        destfile = (file.dest && file.dest !== filepath) ? path.join(file.dest, filename) : filepath,
+        content = grunt.file.read(filepath);
+        content = content.toString(); // sometimes css is interpreted as object
+
+        if (!supportedTypes[type]) { //next
+          console.warn("unrecognized extension:" + type + " - " + filepath);
+          return;
+        }
+
+        grunt.log.subhead('cdn:' + type + ' - ' + filepath);
+
+        if (supportedTypes[type] === "html") {
+          content = html.call(self, content, filepath, relativeTo);
+        } else if (supportedTypes[type] === "css") {
+          content = css.call(self, content, filepath, relativeTo);
+        }
+
+        // write the contents to destination
+        grunt.file.write(destfile, content);
+      });
+    });
+  });
+
+
+
 };
