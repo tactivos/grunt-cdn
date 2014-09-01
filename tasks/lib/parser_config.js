83363f90885e15e342dd0389e0dec3d5a0db9810
Johnny Halife <johnny@mural.ly>
2014-09-01 11:08:36 -0300

    0.5.6

--- tasks/lib/parser_config.js
+++ tasks/lib/parser_config.js
@@ -9,14 +9,18 @@ module.exports = {
       rgx: new RegExp(/(?:href)=['"](?!\w*?:?\/\/)([^'"\{]+)['"].*\/?>/i)
     },
     {
+      splitters: ['<meta '],
+      rgx: new RegExp(/(?:content)=['"](?!\w*?:?\/\/)([^'"\{]+)['"].*\/?>/i)
+    },
+    {
       splitters: ['<script '],
       rgx: new RegExp(/data-main=['"](?!\w*?:?\/\/)([^'"\{]+)['"].*\/?>/i)
     }
   ],
   regcss: new RegExp(/url\(([^)]+)\)/ig),
-  
+
   regcssfilter: new RegExp(/filter[\w\.\:]+\(src=['"]([^'"]+)['"]/ig),
-  
+
   supportedTypes: {
     html: 'html',
     css: 'css',
