17fcba85a9d5064db905d5634d1af5ca401454fc
chemdemo <yangdemo@gmail.com>
2014-09-10 14:26:34 +0800

    ignore `meta` tag

--- tasks/lib/parser_config.js
+++ tasks/lib/parser_config.js
@@ -8,10 +8,10 @@ module.exports = {
       splitters: ['<link '],
       rgx: new RegExp(/(?:href)=['"](?!\w*?:?\/\/)([^'"\{]+)['"].*\/?>/i)
     },
-    {
-      splitters: ['<meta '],
-      rgx: new RegExp(/(?:content)=['"](?!\w*?:?\/\/)([^'"\{]+)['"].*\/?>/i)
-    },
+    // {
+    //   splitters: ['<meta '],
+    //   rgx: new RegExp(/(?:content)=['"](?!\w*?:?\/\/)([^'"\{]+)['"].*\/?>/i)
+    // },
     {
       splitters: ['<script '],
       rgx: new RegExp(/data-main=['"](?!\w*?:?\/\/)([^'"\{]+)['"].*\/?>/i)
